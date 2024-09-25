8c6a90706eb93ed79352254f9561a70a1c93ba5d
EngHirsi <102013843+EngHirsi@users.noreply.github.com>
2024-09-25 09:51:33 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('pivot_table/__init__.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(
         f.read().decode('utf-8')).group(1)))
 
-requirements = parse_requirements("requirements.txt", session="")
+requirements ="requirements.txt"
 
 setup(
 	name='pivot_table',
